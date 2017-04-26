330202fbb283b9276ff08ab0e198693fa67c1d9a
EUR\cgarcia <cgarcia@astrata.eu>
2017-04-26 20:04:45 +0200

    Added main file

---
+++ advanced_lane_lines.py
@@ -0,0 +1,26 @@
+def calibrate_camera() :
+    return
+
+def correct_distortion () :
+    return
+
+def create_threshold_binary () :
+    return
+
+def apply_perspective_transform () :
+    return
+
+def detect_lane_pixels () :
+    return
+
+def fit_lane_boundary () :
+    return
+
+def determine_lane_curvature () :
+    return
+
+def unwarp_image () :
+    return
+
+def output_lane_display ():
+    return
