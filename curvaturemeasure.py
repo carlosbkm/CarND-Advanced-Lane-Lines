c6ecade4bd4e31b37a27e968ead829182fb42b0c
carlosbkm <carlos.garcia.mejias@gmail.com>
2017-05-14 00:22:40 +0200

    Implemented curvature measure

---
+++ curvaturemeasure.py
@@ -0,0 +1,36 @@
+from lanepixelfinding import Lanepixelfinding
+
+__author__ = 'Carlos'
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+
+
+class Curvaturemeasure(object):
+
+    @staticmethod
+    def find_curvature(left_fit, right_fit, y_eval):
+
+        left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+        right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+
+        return left_curverad, right_curverad
+
+
+if __name__ == "__main__":
+
+    laneF = Lanepixelfinding()
+
+    img = mpimg.imread('output_images/binary_threshold/threshold_output.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    left_fit, right_fit = laneF.find_lane_pixels(gray, output_folder='output_images/')
+
+    left_curverad, right_curverad = Curvaturemeasure.find_curvature(left_fit, right_fit,
+                                                                    (gray.shape[0]-1)*Lanepixelfinding.YM_PER_PIX)
+
+    # Now our radius of curvature is in meters
+    print(left_curverad, 'm', right_curverad, 'm')
+    print('Finished main')
