6320e7a322fc3baed545381016469d3f34ec8715
carlosbkm <carlos.garcia.mejias@gmail.com>
2017-05-14 02:51:23 +0200

    Added file with path constants

---
+++ paths.py
@@ -0,0 +1,13 @@
+__author__ = 'Carlos'
+
+OUTPUT_IMAGES_FOLDER = 'output_images/'
+
+BINARY_OUTPUT = 'binary_threshold/'
+CALIBRATION_SOURCE = 'camera_cal/'
+CALIBRATION_OUTPUT = 'calibration_results/'
+CORNERS_FIND_OUTPUT = CALIBRATION_OUTPUT + 'output_corners'
+DISTORTION_OUTPUT = 'distortion_correction/'
+LANES_OUTPUT = 'lane_detection/'
+PERSPECTIVE_OUTPUT = 'perspective_transform/'
+
+CALIBRATION_FILENAME = 'wide_dist_pickle.p'
